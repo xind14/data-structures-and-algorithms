82ac7985cae89d56bd09f23c1a5ed27f93eaa2ab
Xin Deng <xind1470@gmail.com>
2023-11-29 13:15:10 -0800

    Finished 03

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -38,6 +38,8 @@ For example, containsAnd(['panda', 'ran', 'and']) returns ['panda', 'and'].
 
 const containsAnd = (arr) => {
   // Solution code here...
+  let array = [''];
+  return arr.filter((string)=> string.includes('and'))
 };
 
 /* ------------------------------------------------------------------------------------------------
