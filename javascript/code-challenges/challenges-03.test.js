1b3a220c1e95c8586ec47cd3340ad82ef184132a
Xin Deng <xind1470@gmail.com>
2023-11-29 13:22:19 -0800

    Deleted unnecessary arrays

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -38,7 +38,6 @@ For example, containsAnd(['panda', 'ran', 'and']) returns ['panda', 'and'].
 
 const containsAnd = (arr) => {
   // Solution code here...
-  let array = [''];
   return arr.filter((string)=> string.includes('and'))
 };
 
@@ -52,7 +51,6 @@ For example, oddValues([1,2,3]) returns [1,3].
 
 const oddValues = (arr) => {
   // Solution code here...
-  let array =[];
   return arr.filter((number)=>(number%2))
 };
 
@@ -66,6 +64,9 @@ For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 
 const notInFirstArray = (forbiddenValues, arr) => {
   // Solution code here...
+
+  return arr.filter
+
 };
 
 /* ------------------------------------------------------------------------------------------------
