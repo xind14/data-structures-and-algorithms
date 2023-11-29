c0699d4f4f1a248c7a4e60f992c55e54cf94ba30
Xin Deng <xind1470@gmail.com>
2023-11-29 13:27:43 -0800

    Finished 05

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -65,7 +65,7 @@ For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 const notInFirstArray = (forbiddenValues, arr) => {
   // Solution code here...
 
-  return arr.filter
+  return arr.filter((elements)=>!forbiddenValues.includes(elements))
 
 };
 
