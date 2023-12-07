cd7d00b641cb7d957d6e6a2839bd023c9bdd00c3
Xin Deng <xind1470@gmail.com>
2023-12-07 12:56:44 -0800

    finished 2

--- javascript/code-challenges/challenges-09.test.js
+++ javascript/code-challenges/challenges-09.test.js
@@ -41,6 +41,8 @@ Write a function that appends ' The end.' to a string, and returns the modified 
 
 const appendTheEnd = (str) => {
   // Solution code here...
+  let str2 = str + ' The end.'
+  return str2;
 };
 
 /* ------------------------------------------------------------------------------------------------
