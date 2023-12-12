6a00aba9b4a6bace2d07628f8aee7a0330d871aa
Xin Deng <xind1470@gmail.com>
2023-12-12 13:21:05 -0800

    finished 4

--- javascript/code-challenges/challenges-12.test.js
+++ javascript/code-challenges/challenges-12.test.js
@@ -86,6 +86,9 @@ For example, [ [0,2,5,4], [2,4,10], [] ] should return [ [1, 32], [1024], [] ].
 
 const divisibleByFiveTwoToThePower = (input) => {
   // Solution code here...
+  return input.map((array)=>{
+    return array.filter((number)=>typeof number==='number' && number % 5===0).map((number)=>Math.pow(2, number));
+  })
 };
 
 /* ------------------------------------------------------------------------------------------------
