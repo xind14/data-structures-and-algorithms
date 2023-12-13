d73d2559d011dbe98a993facf6810a0c19fc4fd0
Xin Deng <xind1470@gmail.com>
2023-12-13 12:55:58 -0800

    finished 6

--- javascript/code-challenges/challenges-13.test.js
+++ javascript/code-challenges/challenges-13.test.js
@@ -136,6 +136,10 @@ Write a function named citiesAtoJ that takes in an array of city names and uses 
 
 const citiesAtoJ = (arr) => {
   // Solution code here...
+
+  let regex = /^[A-J]/;
+  return arr.filter(city=> regex.test(city));
+
 };
 
 /* ------------------------------------------------------------------------------------------------
