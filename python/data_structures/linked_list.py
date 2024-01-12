10f2af3884184241fd74ecf4a9a45ba820a3278e
Xin Deng <xind1470@gmail.com>
2024-01-12 15:47:04 -0800

    tests passes

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,15 +1,39 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+
 class LinkedList:
     """
     Put docstring here
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.head = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def insert (self, value):
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
+
+    def includes(self, value):
+        current = self.head
+        while current:
+            if current.value == value:
+                return True
+            current = current.next
+        return False
+
+    def __str__(self):
+        result = ""
+        current = self.head
+        while current:
+            result += f"{{ {current.value} }} -> "
+            current = current.next
+        result += "NULL"
+        return result
 
 
 class TargetError:
