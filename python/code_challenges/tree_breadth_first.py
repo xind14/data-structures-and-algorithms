0d45fdb1556f4637cf4acbbd3a0afd658ce686a3
Xin Deng <xind1470@gmail.com>
2024-01-30 14:25:30 -0800

    test pass

--- python/code_challenges/tree_breadth_first.py
+++ python/code_challenges/tree_breadth_first.py
@@ -1,5 +1,21 @@
 from data_structures.binary_tree import BinaryTree
 
 
-def breadth_first():
-    pass
+def breadth_first(tree):
+
+    if tree.root is None:
+        return []
+    list = []
+
+    queue = [tree.root]
+
+    while len(queue) > 0:
+        node = queue.pop(0)
+        list.append(node.value)
+        if node.left is not None:
+            queue.append(node.left)
+        if node.right is not None:
+            queue.append(node.right)
+
+
+    return list
