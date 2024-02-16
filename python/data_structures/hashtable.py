df292fad7d834df3434ba954c7e5c5bb33ccadb6
Xin Deng <xind1470@gmail.com>
2024-02-16 15:05:39 -0800

    tests passes

--- python/data_structures/hashtable.py
+++ python/data_structures/hashtable.py
@@ -1,12 +1,87 @@
+from data_structures.linked_list import LinkedList
+
 class Hashtable:
     """
-    Put docstring here
+    A hashtable implementation.
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, size=1024):
+        """
+        Initializes the hashtable with a specified size.
+        """
+        self._size = size
+        self._buckets = [None] * size
 
-    def some_method(self):
-        # method body here
-        pass
+    def _hash(self, key):
+        """
+        Computes the hash value for the given key.
+        """
+        index = sum(ord(char) for char in key)
+        index = (index * 599) % self._size
+        return index
+
+    def set(self, key, value):
+        """
+        Sets the key-value pair in the hashtable.
+        """
+        index = self._hash(key)
+        bucket = self._buckets[index]
+        if bucket is None:
+            bucket = LinkedList()
+            self._buckets[index] = bucket
+
+        current = bucket.head
+        while current:
+            if current.value[0] == key:
+                current.value[1] = value  # Update value
+                return
+            current = current.next
+
+        bucket.insert([key, value])  # No existing key found, add new entry
+
+    def get(self, key):
+        """
+        Retrieves the value associated with the given key.
+        """
+        index = self._hash(key)
+        bucket = self._buckets[index]
+        if bucket is None:
+            return None
+
+        current = bucket.head
+        while current:
+            if current.value[0] == key:
+                return current.value[1]
+            current = current.next
+
+        return None
+
+
+    def keys(self):
+        """
+        Returns a list of all keys in the hashtable.
+        """
+        key_list = []
+        for bucket in self._buckets:
+            current = bucket.head if bucket else None
+            while current:
+                key_list.append(current.value[0])
+                current = current.next
+        return key_list
+
+
+    def has(self, key):
+        """
+        Checks if the given key exists in the hashtable.
+        """
+        for bucket in self._buckets:
+            current = bucket.head if bucket else None
+            while current:
+                if current.value[0] == key:
+                    return True
+                current = current.next
+        return False
+
+
+
+
--- python/tests/data_structures/test_hashtable.py
+++ python/tests/data_structures/test_hashtable.py
@@ -6,8 +6,29 @@ def test_exists():
     assert Hashtable
 
 
-@pytest.mark.skip("TODO")
-def test_get_apple():
+# @pytest.mark.skip("TODO")
+def test_hash():
+    """
+    NOTE: intentionally breaks "encapsulation" by accessing "internal" attriubutes
+    """
+    hashtable = Hashtable()
+    actual = hashtable._hash("Zach")
+    assert 0 <= actual < hashtable._size
+
+
+# @pytest.mark.skip("TODO")
+def test_hash_twice():
+    """
+    NOTE: intentionally breaks "encapsulation" by accessing "internal" attriubutes
+    """
+    hashtable = Hashtable()
+    first = hashtable._hash("Zach")
+    second = hashtable._hash("Zach")
+    assert first == second
+
+
+# @pytest.mark.skip("TODO")
+def test_apple():
     hashtable = Hashtable()
     hashtable.set("apple", "Used for apple sauce")
     actual = hashtable.get("apple")
@@ -15,7 +36,79 @@ def test_get_apple():
     assert actual == expected
 
 
+# @pytest.mark.skip("TODO")
+def test_apple_again():
+    hashtable = Hashtable()
+    hashtable.set("apple", "Used for apple sauce")
+    hashtable.set("apple", "Can give to teacher")
+    actual = hashtable.get("apple")
+    expected = "Can give to teacher"
+    assert actual == expected
+
+
+# @pytest.mark.skip("TODO")
+def test_key_not_exists():
+    hashtable = Hashtable()
+    actual = hashtable.get("nonexisting key")
+    expected = None
+    assert actual == expected
+
+
+# @pytest.mark.skip("TODO")
+def test_key_not_exists_again():
+    """
+    WARNING: requires that act & cat hash the same
+    """
+    hashtable = Hashtable()
+    hashtable.set("cat", "meow")
+    actual = hashtable.get("act")
+    expected = None
+    assert actual == expected
+
+
+# @pytest.mark.skip("TODO")
+def test_keys():
+    hashtable = Hashtable()
+    hashtable.set("apple", "Used for apple sauce")
+    hashtable.set("banana", "Great in a banana split")
+    actual = hashtable.keys()
+    expected = ["apple", "banana"]
+    assert sorted(actual) == sorted(expected)
+
+
+# @pytest.mark.skip("TODO")
+def test_has():
+    hashtable = Hashtable()
+    hashtable.set("apple", "Used for apple sauce")
+    hashtable.set("banana", "Great in a banana split")
+    assert hashtable.has("apple")
+    assert hashtable.has("banana")
+    assert not hashtable.has("cucumber")
+
+
+# @pytest.mark.skip("TODO")
+def test_keys_repeats():
+    hashtable = Hashtable()
+    hashtable.set("apple", "Used for apple sauce")
+    hashtable.set("apple", "Can give to teacher")
+    hashtable.set("banana", "Great in a banana split")
+    actual = hashtable.keys()
+    expected = ["apple", "banana"]
+    assert sorted(actual) == sorted(expected)
+
+
 @pytest.mark.skip("TODO")
+def test_internals():
+    """
+    NOTE: there's a test_internals in your DSA repo that isn't a great fit.
+    Feel free to ignore it
+    Or tweak it as a STRETCH
+    """
+    pass
+
+
+# gpt test because I have no idea 
+# @pytest.mark.skip("TODO")
 def test_internals():
     hashtable = Hashtable(1024)
     hashtable.set("ahmad", 30)
@@ -24,11 +117,19 @@ def test_internals():
 
     actual = []
 
-    # NOTE: purposely breaking encapsulation to test the "internals" of Hashmap
-    for item in hashtable._buckets:
-        if item:
-            actual.append(item.display())
+    # NOTE: purposely breaking encapsulation to test the "internals" of Hashtable
+    for bucket in hashtable._buckets:
+        if bucket:
+            current = bucket.head
+            bucket_items = []
+            while current:
+                bucket_items.append(current.value)
+                current = current.next
+            actual.append(sorted(bucket_items)) 
 
-    expected = [[["silent", True], ["listen", "to me"]], [["ahmad", 30]]]
+    
+    actual.sort(key=lambda x: x[0][0] if x else None)
+    expected = [[["ahmad", 30]], [["listen", "to me"], ["silent", True]]]
 
     assert actual == expected
+
