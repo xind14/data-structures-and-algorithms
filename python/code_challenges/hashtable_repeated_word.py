89a619f86b7b481ff71e99b58540eb9569e68746
Xin Deng <xind1470@gmail.com>
2024-02-19 14:27:28 -0800

    added code

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -1,5 +1,18 @@
 from data_structures.hashtable import Hashtable
 
+def first_repeated_word(input_string):
+    hashtable = Hashtable()
 
-def first_repeated_word():
-    pass
+    words = [word.strip(string.punctuation) for word in input_string.lower().split()]
+
+    for word in words:
+        if hashtable.get(word):
+            return word
+        else:
+            hashtable.set(word, True)
+
+    return None
+
+import string
+
+
