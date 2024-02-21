c5f48cc7171b01c9735d2fe6fdb36a2fe7e7a155
Xin Deng <xind1470@gmail.com>
2024-02-20 16:31:03 -0800

    added one test

--- python/tests/code_challenges/test_tree_intersection.py
+++ python/tests/code_challenges/test_tree_intersection.py
@@ -8,7 +8,7 @@ def test_exists():
     assert tree_intersection
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_tree_intersection():
 
     tree_a = BinaryTree()
@@ -23,6 +23,18 @@ def test_tree_intersection():
     expected = [125, 175, 100, 160, 500, 200, 350]
 
     assert sorted(actual) == sorted(expected)
+
+def test_empty_tree_intersection():
+    tree_a = BinaryTree()
+    tree_b = BinaryTree()
+    values = [42, 100, 100, 15, 160, 200, 350, 125, 175, 4, 500]
+    add_values_to_empty_tree(tree_b, values)
+
+    actual = tree_intersection(tree_a, tree_b)
+    expected = []
+
+    assert sorted(actual) == sorted(expected)
+
 
 
 def add_values_to_empty_tree(tree, values):
