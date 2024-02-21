19f05ec0f043f7c4c8a057e68b8073498e1f58a2
Xin Deng <xind1470@gmail.com>
2024-02-21 14:43:39 -0800

    added tests

--- python/code_challenges/hashtable_left_join.py
+++ python/code_challenges/hashtable_left_join.py
@@ -1,5 +1,16 @@
 from data_structures.hashtable import Hashtable
 
-
-def left_join():
-    pass
+def left_join(synonyms, antonyms):
+  
+    result = []
+    
+    for key in synonyms.keys():
+        synonym_value = synonyms.get(key)
+        antonym_value = antonyms.get(key)
+        
+        if antonym_value is not None:
+            result.append([key, synonym_value, antonym_value])
+        else:
+            result.append([key, synonym_value, "None"])  
+    
+    return sorted(result)  
